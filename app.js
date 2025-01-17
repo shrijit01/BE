ac5355edf8cd4ac111d030b0894c4b721e93007d
Shrijit_01 <shrijitpandey113@gmail.com>
2025-01-17 15:54:31 +0530

    home page BE

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var ejs = require("ejs");
+// var ejs = require("ejs");
+var cors = require('cors');
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -20,6 +22,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(cors());
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
